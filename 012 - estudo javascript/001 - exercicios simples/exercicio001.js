f3134f2d891eba63f7bfd328c78339d649032bfc
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-03-28 10:37:37 -0300

    resolução guiada do exercício parte 1

---
+++ 012 - estudo javascript/001 - exercicios simples/exercicio001.js
@@ -0,0 +1,18 @@
+/*
+Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 variáveis. Sendo elas:
+1 - Preço do combustivel.
+2 - Gasto médio de combustivel do carro por Km.
+3 - Distância em Km da viagem
+
+Imprima no console o valor que será gasto de combustivel para realizar esta viagem.
+*/
+
+const precoCombustivel = 5.79;
+const kmPorLitros = 10;
+const distanciaEmKm = 100;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros; 
+const valorGasto = litrosConsumidos * precoCombustivel;
+console.log(valorGasto);
