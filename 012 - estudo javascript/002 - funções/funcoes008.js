40ca6176208b431df30c589021181a21986e36e8
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-04-08 18:08:07 -0300

    1º exemplo função escreva seu nome

---
+++ 012 - estudo javascript/002 - funções/funcoes008.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+//Função escreva seu nome
+
+function escrevaMeuNome(nome) {
+  console.log(`Seu nome é: ${nome}`);
+}
+escrevaMeuNome("Marcelo");
+escrevaMeuNome("Safira");
