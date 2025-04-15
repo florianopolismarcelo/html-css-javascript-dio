0cc7753923248dc7bb3d67cf4ed959b4944e75c4
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-04-15 12:03:58 -0300

    estrutura básica de um objeto

---
+++ 012 - estudo javascript/003 - objetos/objetos001.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+const safira = {
+  nome: 'Safira a gatinha',
+  idade: 2 
+};
+
+console.log(`${safira.nome}`)
+console.log(`${safira.idade}`)
+console.log(safira)
+
+
+ 
