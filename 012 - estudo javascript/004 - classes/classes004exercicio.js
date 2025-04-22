8363be673da88e6d9a9ab13fe940161d56705168
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-04-22 17:49:26 -0300

    exercício guiado - criando a classe carro

---
+++ 012 - estudo javascript/004 - classes/classes004exercicio.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+/*Crie uma classe para representar carros.
+Os carros possuiem uma marca, uma cor e um gasto médio de combustível por kilomentros rodados.
+Crie um método que dado a quantidade de quilômetros e o preço do combustível nos dê o valor gasto em reais para realizar este percurso.
+*/
+
+class Carro {
+  marca;
+  cor;
+  gastoMedioPorKm;
+
+  constructor(marca, cor, gastoMedioPorKm){
+    this.marca = marca;
+    this.cor = cor;
+    this.gastoMedioPorKm = gastoMedioPorKm;
+  }
+}
+
