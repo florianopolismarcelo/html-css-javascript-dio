a391ec5199cfb4a73cce88025be276e2fbae9dbb
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-04-23 11:57:21 -0300

    exercício guiado - criando a classe pessoa

---
+++ 012 - estudo javascript/004 - classes/classes005exercicio.js
@@ -0,0 +1,19 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+/*Crie uma classe para representar pessoas.
+Para cada pessoa teremos os atributos nome, peso e altura.
+As pessoas devem ter a capacidade de dizer o valor do seu IMC (IMC = peso / (altura * altura))
+Instancie uma pessoa clamada José que tenha 70kg de peso e 1,75 de altura e peça para José dizer o valor do seu IMC
+*/
+
+class Pessoa {
+  nome;
+  peso;
+  altura;
+
+  constructor(nome, peso, altura) {
+    this.nome = nome;
+    this.peso = peso;
+    this.altura = altura;
+  }
+}
