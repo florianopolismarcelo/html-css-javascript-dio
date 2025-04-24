565a88c23c1ae372e6c07c278b76efe92e479cff
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-04-24 16:17:48 -0300

    execultando for em uma string

---
+++ 012 - estudo javascript/005 - arrays/arrays005.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+const nome = "Safira Marcelo Alexandre"
+for (let i = 0; i < nome.length; i++) {
+  console.log(nome[i]);
+}
+
+/*
+abaixo é um exemplo com a atribuição da variável letra
+
+const nome = "Safira Marcelo Alexandre";
+for (let i = 0; i < nome.length; i++) {
+  const letra = nome[i]
+  console.log(letra);
+}
+*/
