5e226cfc1491fb6a56368b1c64a994b7f480aab4
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-04-24 15:11:34 -0300

    o que é um array

---
+++ 012 - estudo javascript/005 - arrays/arrays001.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+const alunos = ['Marcelo', 'Safira', 'Alexandre']
+console.log(alunos)
+console.log(alunos[2])
+console.log(alunos[1])
+console.log(alunos[0])
