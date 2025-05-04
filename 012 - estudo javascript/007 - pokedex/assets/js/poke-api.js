61ec6898e8f4a280e632597efb629c25711fd37c
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-05-04 13:12:35 -0300

    convertendo o model do pokeapi para nosso model

--- 012 - estudo javascript/007 - pokedex/assets/js/poke-api.js
+++ 012 - estudo javascript/007 - pokedex/assets/js/poke-api.js
@@ -1,9 +1,29 @@
 
 const pokeApi = {};
 
-pokeApi.getPokemonDetail = (pokemon) => {
- return fetch(pokemon.url).then((response) => response.json())
+function convertPokeApiDetailToPokemon(pokeDetail) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.order;
+  pokemon.name = pokeDetail.name;
+
+ const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name); 
+ const [type] = types; // Destructuring assignment to get the first type
+
+  pokemon.types = types;
+  pokemon.type = type;
+
+
+ pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+
+ return pokemon;
+
 }
+pokeApi.getPokemonDetail = (pokemon) => {
+ return fetch(pokemon.url)
+         .then((response) => response.json())
+         .then(convertPokeApiDetailToPokemon) 
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
   
@@ -12,7 +32,7 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
     .then((jsonBody) => jsonBody.results)
     .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
     .then((detailRequests) => Promise.all(detailRequests))
-    then((pokemonsDetails) => pokemonsDetails)
+    .then((pokemonsDetails) => pokemonsDetails)
      //.catch((error) => console.error(error));
 }
 
