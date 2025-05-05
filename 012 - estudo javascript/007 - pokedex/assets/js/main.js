433e43541e5bbfc9b6fdab2ba9677a598a3b1f41
florianopolismarcelo <florianopolismarcelo@msn.com>
2025-05-05 12:21:26 -0300

    criando mecanismo de paginação

--- 012 - estudo javascript/007 - pokedex/assets/js/main.js
+++ 012 - estudo javascript/007 - pokedex/assets/js/main.js
@@ -1,6 +1,8 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMoreButton");
-const limit = 5;
+
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
 function loadPokemonItens(offset, limit) {
@@ -28,7 +30,15 @@ loadPokemonItens(offset, limit);
 
 loadMoreButton.addEventListener("click", () => {
   offset += limit;
-  loadPokemonItens(offset, limit);
+  const qtdRecordWithNexPage = offset + limit
+
+  if (qtdRecordWithNexPage >= maxRecords) {
+    const newLimit = maxRecords - offset;
+    loadPokemonItens(offset, newLimit);
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  } else {
+    loadPokemonItens(offset, limit);
+  }
 });
 
 /*
